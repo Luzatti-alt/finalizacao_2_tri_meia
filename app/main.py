b4ed9e19ca569b4fe72c1440a30575df3d100bbf
Luzatti-alt <lucasguzatti4@gmail.com>
2025-07-23 09:07:54 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -7,25 +7,30 @@ class Prototipo(App):
     def build(self):
         altura = Window.height
         largura = Window.width
-
         layout = FloatLayout()
-        IA = Button(
-            text='IA geradora de imagem',
-            size_hint=(None, None),  # Tamanho fixo, sem resize automático
-            size=(200, 50),
-            pos=(largura-200, altura-50)  # Corrigido para centralizar
-        )
 
-        # Criando o botão com a posição corrigida
-        enviar = Button(
-            text='Enviar design',
-            size_hint=(None, None),  # Tamanho fixo, sem resize automático
-            size=(200, 50),
-            pos=(largura / 2 - 100, altura / 8 - 25)  # Corrigido para centralizar
-        )
+        # onde sera desenhada a img e componentes desta tela
+        def draw():
+            # Botão da IA para gerar imagem
+            IA = Button(
+                text='IA geradora de imagem',
+                size_hint=(None, None),
+                size=(200, 50),
+                pos=(largura - 200, altura - 50)
+            )
+            IA.bind(on_press=self.gen_img)  # Ação ao pressionar o botão
+            enviar = Button(
+                text='Enviar design',
+                size_hint=(None, None),  # Tamanho fixo, sem resize automático
+                size=(200, 50),
+                pos=(largura / 2 - 100, altura / 8 - 25)  # Corrigido para centralizar
+            )
+            layout.add_widget(enviar)  # Adicionando o botão ao layout
+            layout.add_widget(IA)  # Adicionando o botão IA ao layout
+        draw()  # Chamando a função para desenhar os componentes
 
-        layout.add_widget(enviar)  # Adicionando o botão ao layout
-        layout.add_widget(IA)#botão da ia que gerar a img
         return layout
 
+    def gen_img(self, instance):
+        # onde sera gerado a img por ia e componentes desta tela
 Prototipo().run()
