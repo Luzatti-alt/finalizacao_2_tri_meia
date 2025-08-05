7f23e1f9a74e3567aae3b5538376f28d5fccedf0
Luzatti-alt <lucasguzatti4@gmail.com>
2025-08-05 11:11:14 -0300

    Update db.py

--- app/db.py
+++ app/db.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, String , Integer,Float, Boolean
+from sqlalchemy import create_engine, Column, String , Integer,Float, Boolean,UniqueConstraint
 from sqlalchemy.orm import sessionmaker, declarative_base
 #criar db
 db = create_engine("sqlite:///Meia_controle_fios.db")#tipo de sql:///nome do db.db
@@ -12,11 +12,10 @@ class Cores(Base):
   cor = Column("cor",String)
   quantidade_cor_kg = Column("quantidade_cor_kg",Float)
   disponivel = Column("disponivel",Boolean)
+  __table_args__ = (UniqueConstraint('cor', name='uq_cores_cor'),)
   def __int__(self,cor,quantidade_cor_kg,disponivel = True):
     self.cor = cor
     self.quantidade_cor_kg = quantidade_cor_kg
     self.disponivel = disponivel
-
 #criar o arquivo do db com o que precisa
 Base.metadata.create_all(bind=db)#ele cria o arquivo do db
-Cores(cor="vermelho",quantidade_cor_kg="100",disponivel=True)
