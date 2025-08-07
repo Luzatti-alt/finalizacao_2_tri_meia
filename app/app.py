8fcc1f6d7c9e982cd3488ca53723bdade6771553
Luzatti-alt <lucasguzatti4@gmail.com>
2025-08-07 19:26:27 -0300

    Atualizar o app.py

--- app/app.py
+++ app/app.py
@@ -14,6 +14,7 @@ class PrototipoApp(App):
         self.state = 0
         self.tipo_acao = ""
         confirmacao = ["sim", "s", "ss", "positivo", "afirmativo"]
+
         def ver_estoque():
             self.pergunta.text = ""
             cores_no_db = session.query(Cores).all()
@@ -24,54 +25,55 @@ class PrototipoApp(App):
                 ))
 
         def ao_enviar(instance):
-        	if self.tipo_acao == "add_cor":
-        		if self.state == 1:
-        			self.nv_cor = self.entrada.text.strip().lower()  # padroniza 
-        			if self.nv_cor == "":
-        			     self.pergunta.text = "Digite uma cor válida!"
-        			     return
-        			     existe = session.query(Cores).filter_by(cor=self.nv_cor).first()
-        			     if existe:
-        			         self.grid.add_widget(Label(text="Cor já existe no sistema",
-                    		 size_hint_y=None, height=30, color=(1, 1, 1, 1)
-                    		 ))
-                    		 self.tipo_acao = ""
-                    		 self.state = 0
-                    		 self.pergunta.text = ""
-                    		 self.entrada.text = ""
-                    		 return
-            self.entrada.text = ""
-            self.pergunta.text = "Digite quantos quilos desta cor (somente número):"
-            self.state = 2
+            if self.tipo_acao == "add_cor":
+                if self.state == 1:
+                    self.nv_cor = self.entrada.text.strip().lower()  # padroniza 
+                    if self.nv_cor == "":
+                        self.pergunta.text = "Digite uma cor válida!"
+                        return
+                        existe = session.query(Cores).filter_by(cor=self.nv_cor).first()
+                        if existe:
+                            self.grid.add_widget(Label(text="Cor já existe no sistema",
+                                size_hint_y=None, height=30, color=(1, 1, 1, 1)
+                            ))
+                            self.tipo_acao = ""
+                            self.state = 0
+                            self.pergunta.text = ""
+                            self.entrada.text = ""
+                            return
+                    self.entrada.text = ""
+                    self.pergunta.text = "Digite quantos quilos desta cor (somente número):"
+                    self.state = 2
 
-        elif self.state == 2:
-            qnt_text = self.entrada.text.strip()
-            try:
-                self.qnt_cor = float(qnt_text)
-            except ValueError:
-                self.pergunta.text = "Quantidade inválida! Digite um número válido."
-                return
+                elif self.state == 2:
+                    qnt_text = self.entrada.text.strip()
+                    try:
+                        self.qnt_cor = float(qnt_text)
+                    except ValueError:
+                        self.pergunta.text = "Quantidade inválida! Digite um número válido."
+                        return
 
-            nova_cor = Cores(cor=self.nv_cor, quantidade_cor_kg=self.qnt_cor, disponivel=True)
-            session.add(nova_cor)
-            try:
-                session.commit()
-            except Exception as e:
-                session.rollback()
-                self.grid.add_widget(Label(
-                    text=f"Erro ao adicionar cor: {e}",
-                    size_hint_y=None, height=30, color=(1, 0, 0, 1)
-                ))
-                return
+                    nova_cor = Cores(cor=self.nv_cor, quantidade_cor_kg=self.qnt_cor, disponivel=True)
+                    session.add(nova_cor)
+                    try:
+                        session.commit()
+                    except Exception as e:
+                        session.rollback()
+                        self.grid.add_widget(Label(
+                            text=f"Erro ao adicionar cor: {e}",
+                            size_hint_y=None, height=30, color=(1, 0, 0, 1)
+                        ))
+                        return
 
-            self.grid.add_widget(Label(
-                text=f"Cor '{self.nv_cor}' adicionada com {self.qnt_cor} kg.",
-                size_hint_y=None, height=30, color=(0, 1, 0, 1)
-            ))
-            self.state = 0
-            self.tipo_acao = ""
-            self.pergunta.text = ""
-            self.entrada.text = ""
+                    self.grid.add_widget(Label(
+                        text=f"Cor '{self.nv_cor}' adicionada com {self.qnt_cor} kg.",
+                        size_hint_y=None, height=30, color=(0, 1, 0, 1)
+                    ))
+                    self.state = 0
+                    self.tipo_acao = ""
+                    self.pergunta.text = ""
+                    self.entrada.text = ""
+
         def add_cor():
             self.pergunta.text = "Digite a nova cor a ser adicionada:"
             self.tipo_acao = "add_cor"
@@ -110,88 +112,7 @@ class PrototipoApp(App):
                 self.pergunta.text = "Digite o quanto será atualizado(em kg):"
                 qnt_upt = float(self.entrada.text)
             except ValueError:
-                self.grid.add_widget(Label(text="Valor inválido. Digite um número válido", size_hint_y=None, height=30, color=(1, 1, 1, 1)))
-                return
-            cor_obj = cores_disponiveis[qual_upt]
-            if tipo_upt == "remover":
-                if cor_obj.quantidade_cor_kg - qnt_upt < 0:
-                    self.grid.add_widget(Label(text=f"Erro: Não é possível remover {qnt_upt} kg, só há {cor_obj.quantidade_cor_kg} kg no estoque", size_hint_y=None, height=30, color=(1, 1, 1, 1)))
-                    return
-                cor_obj.quantidade_cor_kg -= qnt_upt
-                self.grid.add_widget(Label(text=f"Removido {qnt_upt} kg de {qual_upt}. Novo total: {cor_obj.quantidade_cor_kg:.2f}kg", size_hint_y=None, height=30, color=(1, 1, 1, 1)))
-            elif tipo_upt == "renovar":
-                cor_obj.quantidade_cor_kg += qnt_upt
-                self.grid.add_widget(Label(text=f"Adicionado {qnt_upt} kg da cor {qual_upt}. Novo total: {cor_obj.quantidade_cor_kg:.2f} kg", size_hint_y=None, height=30, color=(1, 1, 1, 1)))
-            session.commit()
-
-        altura = Window.height
-        largura = Window.width
-        layout = FloatLayout()
-
-        with layout.canvas.before:
-            Color(0.1, 0.1, 0.2, 1)
-            self.bg_rect = Rectangle(pos=layout.pos, size=Window.size)
-
-        def update_bg(*args):
-            self.bg_rect.pos = layout.pos
-            self.bg_rect.size = Window.size
-
-        layout.bind(pos=update_bg, size=update_bg)
-
-        # Botões
-        self.estoque = Button(text='Ver estoque', size_hint=(None, None), size=(300, 100), background_normal="", background_color=(0.2, 0.6, 0.9, 1), pos=(0, altura / 2 + 200))
-        self.add_cor = Button(text='Adicionar cor', size_hint=(None, None), size=(300, 100), background_normal="", background_color=(0.2, 0.6, 0.9, 1), pos=(0, altura / 2 + 80))
-        self.remover_cor = Button(text='Remover cor', size_hint=(None, None), size=(300, 100), background_normal="", background_color=(0.2, 0.6, 0.9, 1), pos=(0, altura / 2 - 40))
-        self.prod_meia = Button(text='Produzir meia', size_hint=(None, None), size=(300, 100), background_normal="", background_color=(0.2, 0.6, 0.9, 1), pos=(0, altura / 2 - 160))
-        self.upt_cor = Button(text='Atualizar cor', size_hint=(None, None), size=(300, 100), background_normal="", background_color=(0.2, 0.6, 0.9, 1), pos=(0, altura / 2 - 280))
-        self.enviar = Button(text='enviar', size_hint=(None, None), size=(300, 100), background_normal="", background_color=(0.2, 0.6, 0.9, 1), pos=(0, altura / 2 - 280))
-
-        layout.add_widget(self.enviar)
-        layout.add_widget(self.estoque)
-        layout.add_widget(self.add_cor)
-        layout.add_widget(self.remover_cor)
-        layout.add_widget(self.prod_meia)
-        layout.add_widget(self.upt_cor)
-
-        self.estoque.bind(on_press=lambda instance: ver_estoque())
-        self.add_cor.bind(on_press=lambda instance: add_cor())
-        self.remover_cor.bind(on_press=lambda instance: remove_cor())
-        self.prod_meia.bind(on_press=lambda instance: produzir_meia())
-        self.upt_cor.bind(on_press=lambda instance: upt_fio())
-        self.enviar.bind(on_press=ao_enviar)
-
-        # Painel azul de fundo para scroll
-        with layout.canvas.before:
-            Color(0.1, 0.3, 0.8, 1)
-            self.painel_fundo = Rectangle()
-
-        self.scroll = ScrollView(size_hint=(None, None))
-        self.grid = GridLayout(cols=1, size_hint_y=None, padding=10, spacing=10)
-        self.grid.bind(minimum_height=self.grid.setter("height"))
-        self.scroll.add_widget(self.grid)
-        layout.add_widget(self.scroll)
-
-        self.pergunta = Label(text="Pergunta:", size_hint=(None, None), color=(1, 1, 1, 1))
-        layout.add_widget(self.pergunta)
-
-        self.entrada = TextInput(hint_text="Digite aqui...", size_hint=(None, None), background_color=(0, 0, 0, 1), foreground_color=(1, 1, 1, 1), cursor_color=(1, 1, 1, 1))
-        layout.add_widget(self.entrada)
-
-        Window.bind(size=self.reposicionar_elementos)
-        self.reposicionar_elementos()
-        return layout
-
-    def reposicionar_elementos(self, *args):
-        altura = Window.height
-        largura = Window.width
-        self.estoque.pos = (0, altura / 2 + 200)
-        self.add_cor.pos = (0, altura / 2 + 80)
-        self.remover_cor.pos = (0, altura / 2 - 40)
-        self.prod_meia.pos = (0, altura / 2 - 160)
-        self.upt_cor.pos = (0, altura / 2 - 280)
-        self.enviar.pos = (largura - 125, altura / 10)
-        self.enviar.size = (100, altura / 10)
-        self.pergunta.pos = (340, altura / 8)
+                self.grid.add_widget(Label(text="Valor inválido. Digite um número válido", size_hint altura / 8)
         self.pergunta.size = (largura - 360, altura / 10 + 200)
         self.entrada.pos = (340, altura / 10)
         self.entrada.size = (largura - 460, altura / 10)
