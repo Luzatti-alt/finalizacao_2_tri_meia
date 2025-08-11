4dbf7762c538568bee8796f6c2e5113411ebb61a
Luzatti-alt <lucasguzatti4@gmail.com>
2025-08-11 09:34:25 -0300

    Update servidor.py

--- web/servidor.py
+++ web/servidor.py
@@ -1,16 +1,18 @@
 from flask import Flask, render_template
-
+#impelmentar o db
+from app.db import session, Cores, usuarios
 app = Flask(__name__)
+#integrar a um db ja existente
 @app.route("/")#home page
 def home():
 	return render_template("home.html")#retornar o arquivo html de home
 @app.route("/info")
 def info():
     return render_template("informação.html")
-@app.route("/produto)")
+@app.route("/produto")
 def produto():
     return render_template("produto.html")
 @app.route("/adm")
 def adm():
-    return render_template("adm.html")
+    return render_template("adm.html",Cores=Cores,usuarios=usuarios)
 app.run(host="0.0.0.0",port=5000)
