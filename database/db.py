85ead828c2a4276642a06f58a254f2d0a64305d1
Luzatti-alt <lucasguzatti4@gmail.com>
2025-08-25 10:15:53 -0300

    add produtos no db

--- database/db.py
+++ database/db.py
@@ -35,6 +35,11 @@ class usuarios(Base):
   User = Column("Usuario",String)
   senha = Column("senha",String)
   cargo = Column(String)
+class Produtos(Base):
+  __tablename__ = 'Usuarios'
+  id = Column("id",Integer,primary_key=True, autoincrement=True)#chave principal & qnd add uma nova cor ele ja cria um novo id
+  Nome_prod = Column("nome produto",String)
+  quantidade_prod = Column("quantidade produto",int)
 #criar o arquivo do db com o que precisa
 if not os.path.exists(DB_PATH):
     print("Banco não encontrado, criando novo em:", DB_PATH)
